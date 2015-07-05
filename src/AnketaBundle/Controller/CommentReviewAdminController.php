f45841aecf51d7566aba17ad374c08f56a441bdb
mr.Shu <mr@shu.io>
2015-07-05 02:18:18 +0200

    update: Vymazane nepotrebne veci
    * Vymazane mrtve kusy kodu, ktore sa dostali do codebase zbesilym
      kopirovanim.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- src/AnketaBundle/Controller/CommentReviewAdminController.php
+++ src/AnketaBundle/Controller/CommentReviewAdminController.php
@@ -14,11 +14,6 @@ namespace AnketaBundle\Controller;
 
 use Doctrine\DBAL\DBALException;
 
-use AnketaBundle\Entity\TeachersSubjects;
-use AnketaBundle\Entity\User;
-use AnketaBundle\Entity\TeachingAssociation;
-use AnketaBundle\Entity\UserSeason;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
@@ -85,10 +80,10 @@ class CommentReviewAdminController extends Controller {
         $answers = $em->getRepository('AnketaBundle\Entity\Answer');
         foreach ($comments as $id => $comment) {
             if (!isset($comment['inappropriate'])) {
-                $comment['inappropriate'] = 0; 
+                $comment['inappropriate'] = 0;
             }
             if (!isset($comment['reviewed'])) {
-                $comment['reviewed'] = 0; 
+                $comment['reviewed'] = 0;
             }
 
             $inapp = $comment['inappropriate'];
@@ -104,71 +99,12 @@ class CommentReviewAdminController extends Controller {
             } else if ($reviewed != $prev_reviewed) {
                 $answer = $answers->findOneBy(array('id' => $id));
                 $answer->setReviewed($reviewed);
-                 
+
                 $em->flush();
             }
         }
         return $this->redirect(
                 $this->generateUrl('admin_comments_review',
                                    array('page' => $page)));
-    }
-
-    /**
-     * Links the teacher with the subject as reported in particular
-     * TeachingAssociation.
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    private function addTeacherToSubject(TeachingAssociation $ta) {
-        $em = $this->getDoctrine()->getManager();
-
-        // at least one of the functions should to be set to 1 (true)
-        $is_function_set = $ta->getTrainer() || $ta->getTeacher();
-
-        if ($ta->getTeacher() !== null && $ta->getSubject() !== null
-                && $ta->getSeason() !== null && $is_function_set) {
-
-            // check for duplication
-            $userSeason = $em->getRepository('AnketaBundle:UserSeason')
-                    ->findOneBy(array('season' => $ta->getSeason(),
-                                      'user'   => $ta->getTeacher()));
-            if ($userSeason === null) {
-                $userSeason = new UserSeason();
-                $userSeason->setIsStudent(false);
-                $userSeason->setIsTeacher(true);
-                $userSeason->setSeason($ta->getSeason());
-                $userSeason->setUser($ta->getTeacher());
-            } else {
-                $userSeason->setIsTeacher(true);
-            }
-            $em->persist($userSeason);
-            $em->flush();
-
-            // link the teacher with the subject
-            $teachersSubjects = new TeachersSubjects($ta->getTeacher(),
-                    $ta->getSubject(), $ta->getSeason());
-
-            $teachersSubjects->setLecturer($ta->getLecturer());
-            $teachersSubjects->setTrainer($ta->getTrainer());
-
-            $session = $this->get('session');
-
-            try {
-                $em->persist($teachersSubjects);
-                $em->flush();
-            } catch (DBALException $e) {
-                // TODO check if $e really says the insert is duplicated (SQL 23000)
-                $session->getFlashBag()
-                        ->add('error', 'Učiteľ už je priradený k predmetu.');
-                return $this->redirect($this->generateUrl(
-                        'admin_teaching_associations'));
-            }
-
-            $session->getFlashBag()
-                    ->add('success', 'Učiteľ bol úspešne priradený k predmetu.');
-
-            return $this->redirect($this->generateUrl(
-                    'admin_teaching_associations'));
-        }
     }
 }
